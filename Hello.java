140390f36ee46761952607a8a60c93d13bcf72e6
Terrazasgd <46800701+Terrazasgd@users.noreply.github.com>
2019-01-17 19:41:42 -0500

    Add files via upload
    Lab 1 CMSC 255

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/********************************************************
+* Java Program Name 
+*********************************************************
+* Project/Program description
+*________________________________________________________
+* Gery Terrazas
+* 1/16/2019
+* CMSC 255 Sec 01
+*********************************************************/
+
+ public class Hello { 
+  public static void printHeading() {
+   System.out.println("Gery Terrazas"); 
+   System.out.println("Project 1"); 
+   System.out.println("1/16/2019");
+   System.out.println("CMSC 255 Sec 01");
+   System.out.println();
+  }
+  
+  public static void main(String[] args){
+   printHeading();
+   System.out.println("Hello");
+  }
+ }
